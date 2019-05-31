8c1b499af9bb63fa2f7583ca5e5bf0d9bacf2239
dhurken <conan.stuart+github@gmail.com>
2019-05-30 23:15:13 -0500

    included applicant selector fn

--- Grade_From_GPA.py
+++ Grade_From_GPA.py
@@ -14,3 +14,15 @@ def grade_converter(gpa):
 ltrGrade = grade_converter(4.01)
 
 print(ltrGrade)
+
+def applicant_selector(gpa,ps_score,ec_count):
+  if gpa >= 3.0 and ps_score >= 90 and ec_count >= 3:
+    return "This applicant should be accepted."
+  elif gpa >= 3.0 and ps_score >= 90 and ec_count < 3:
+    return "This applicant should be given an in-person interview."
+  else:
+      return "This applicant should be rejected."
+
+stuApp = applicant_selector(3.0,9,2.0)
+
+print(stuApp)
